41899d8605176a2d9caa2c36b941acf76b4ddd7b
Connor Smiley <connor.smiley@uoit.net>
2021-07-22 14:16:59 -0400

    Simplified listeners and overall flow

--- JavaScript.js
+++ JavaScript.js
@@ -1,57 +1,52 @@
 const init = () => {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	var w = canvas.width;
-	var h = canvas.height;
-	var canvasEdges = canvas.getBoundingClientRect();
-	var mx, my;
-	var keysDown = [];
-	var fps = Math.floor(1000/60);
-	var now, then, delta;
+	let canvas = document.getElementById("canvas");
+	let ctx = canvas.getContext("2d");
+	let w = canvas.width;
+	let h = canvas.height;
+	let mouse = { x: 0, y: 0 };
+	let keysDown = [];
+	let fps = Math.floor(1000/60);
+	let colour = 'red';
 	
-	function gameLoop(){
-		now=Date.now();
-		ctx.clearRect(0,0,w,h);
-		ctx.fillStyle='white';	//change colour
-		ctx.fillRect(0,0,w,h);	//draw rectangle
+	function gameLoop() {
+		let frameBegin = Date.now();
 		
-		//code goes here
-		ctx.fillStyle='red';
-		if(keysDown[32])
-			ctx.fillStyle='green';
-		ctx.fillRect(w/2,h/2,12,48);
-		console.log(mx);
-		console.log(my);
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0, 0, w, h);
 		
-		then=Date.now();
-		delta=then-now;
-		setTimeout(requestAnimationFrame(gameLoop), fps-delta);
+		if (keysDown[32])
+			colour = 'blue';
+		ctx.fillStyle = colour;
+		ctx.fillRect(mouse.x - 20, mouse.y - 30, 40, 60);
+		
+		setTimeout(requestAnimationFrame(gameLoop), fps - (Date.now() - frameBegin));
 	}
 	requestAnimationFrame(gameLoop);
-	
-	canvas.addEventListener('click', function (evt) {
-		//console.log("Click!");
-	}, false);
 
-	canvas.addEventListener('mousemove', function(evt) {
-		//console.log("Aids!");
-		getMousePos(canvas, evt);
+	canvas.addEventListener('mousemove', e => {
+		mouse.x = e.offsetX;
+		mouse.y = e.offsetY;
+	});
+
+	canvas.addEventListener('mousedown', e => {
+		colour = 'green';
+	});
+
+	canvas.addEventListener('mouseup', e => {
+		colour = 'red';
+	});
+	
+	canvas.addEventListener('click', e => {
+		console.log("Click!");
 	}, false);
 	
-	window.addEventListener('keydown', function(evt) {
-		var keyDown = evt.keyCode;
-		keysDown[keyDown]=true;
+	window.addEventListener('keydown', e => {
+		var keyDown = e.keyCode;
+		keysDown[keyDown] = true;
 	}, false);
 	
-	window.addEventListener('keyup', function(evt) {
-		var keyUp = evt.keyCode;
-		keysDown[keyUp]=false;
+	window.addEventListener('keyup', e => {
+		var keyUp = e.keyCode;
+		keysDown[keyUp] = false;
 	}, false);
-	
-	function getMousePos(canvas, evt) {
-		//console.log(evt.clientX);
-		mx = evt.clientX - canvasEdges.left;
-		my = evt.clientY - canvasEdges.top;
-	}
-	
 }
