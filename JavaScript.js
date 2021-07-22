76d62e0a92d0d4088622ec9e20c81cac40157414
Connor Smiley <connor.smiley@uoit.net>
2021-07-22 16:38:34 -0400

    Made a Scene class to handle events

--- JavaScript.js
+++ JavaScript.js
@@ -2,34 +2,70 @@ const init = () => {
 	//Animation variables.
 	const targetFrameRate = 60;
 	const millisecondsPerFrame = 1000 / targetFrameRate;
-	let deltaTime = millisecondsPerFrame;
+	let previousDeltaTime = millisecondsPerFrame;
 
 	//Surface variables.
-	let canvas = document.getElementById("canvas");
-	let ctx = canvas.getContext("2d");
-	let w = canvas.width;
-	let h = canvas.height;
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+	const w = canvas.width;
+	const h = canvas.height;
 
 	//Input variables.
 	let mouse = { x: 0, y: 0 };
-	let keysDown = [];
-	let colour = 'red';
+	let keys = [];
+
+	const SceneType = {
+		invalid: -1,
+		main: 0,
+		keyboard: 1,
+		mouse: 2
+	}
+	
+	class Scene {
+		constructor() {}
+
+		type() {
+			return SceneType.invalid;
+		}
+	
+		onUpdate(deltaTime) {}
+		onKeyDown(key) {}
+		onKeyUp(key) {}
+		onMouseMove(x, y) {}
+		onMouseDown() {}
+		onMouseUp() {}
+		onClick() {}
+	}
+	
+	class MainScene extends Scene {
+		constructor() {
+			super();
+			this.colour = 'red';
+		}
+	
+		onUpdate(deltaTime) {
+			//"Clear" previous frame by rendering over its entirety.
+			ctx.fillStyle = 'white';
+			ctx.fillRect(0, 0, w, h);
+	
+			//Keyboard and mouse test.
+			if (keys[32])
+				this.colour = 'blue';
+			ctx.fillStyle = this.colour;
+			ctx.fillRect(mouse.x - 20, mouse.y - 30, 40, 60);
+			ctx.fillText(deltaTime, 100, 100);
+		}
+	}
+
+	//Game variables.
+	let mainScene = new MainScene();
+	let scene = mainScene;
 	
 	//timestamp is assigned internally by requestAnimationFrame. It stores the time in milliseconds since the document loaded at which requestAnimationFrame was executed.
 	function gameLoop(timestamp) {
-		//"Clear" previous frame by rendering over its entirety.
-		ctx.fillStyle = 'white';
-		ctx.fillRect(0, 0, w, h);
-		
-		//Keyboard and mouse test.
-		if (keysDown[32])
-			colour = 'blue';
-		ctx.fillStyle = colour;
-		ctx.fillRect(mouse.x - 20, mouse.y - 30, 40, 60);
-
-		//Update timers and restart issue a request to render the pending frame.
-		deltaTime = performance.now() - timestamp;
-		setTimeout(requestAnimationFrame(gameLoop), millisecondsPerFrame - deltaTime);
+		scene.onUpdate(previousDeltaTime);
+		previousDeltaTime = performance.now() - timestamp;
+		setTimeout(requestAnimationFrame(gameLoop), millisecondsPerFrame - previousDeltaTime);
 	}
 	requestAnimationFrame(gameLoop);
 
@@ -39,11 +75,11 @@ const init = () => {
 	});
 
 	canvas.addEventListener('mousedown', e => {
-		colour = 'green';
+		console.log("Mouse down!");
 	});
 
 	canvas.addEventListener('mouseup', e => {
-		colour = 'red';
+		console.log("Mouse up!");
 	});
 	
 	canvas.addEventListener('click', e => {
@@ -52,11 +88,11 @@ const init = () => {
 	
 	window.addEventListener('keydown', e => {
 		var keyDown = e.keyCode;
-		keysDown[keyDown] = true;
+		keys[keyDown] = true;
 	}, false);
 	
 	window.addEventListener('keyup', e => {
 		var keyUp = e.keyCode;
-		keysDown[keyUp] = false;
+		keys[keyUp] = false;
 	}, false);
 }
