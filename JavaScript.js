d68b4c716d71b84dcc566488b55836ad1ed69f6f
Connor Smiley <connor.smiley@uoit.net>
2021-07-22 15:01:40 -0400

    Improved animation requests and frame delta time

--- JavaScript.js
+++ JavaScript.js
@@ -1,25 +1,35 @@
 const init = () => {
+	//Animation variables.
+	const targetFrameRate = 60;
+	const millisecondsPerFrame = 1000 / targetFrameRate;
+	let deltaTime = millisecondsPerFrame;
+
+	//Surface variables.
 	let canvas = document.getElementById("canvas");
 	let ctx = canvas.getContext("2d");
 	let w = canvas.width;
 	let h = canvas.height;
+
+	//Input variables.
 	let mouse = { x: 0, y: 0 };
 	let keysDown = [];
-	let fps = Math.floor(1000/60);
 	let colour = 'red';
 	
-	function gameLoop() {
-		let frameBegin = Date.now();
-		
+	//timestamp is assigned internally by requestAnimationFrame. It stores the time in milliseconds since the document loaded at which requestAnimationFrame was executed.
+	function gameLoop(timestamp) {
+		//"Clear" previous frame by rendering over its entirety.
 		ctx.fillStyle = 'white';
 		ctx.fillRect(0, 0, w, h);
 		
+		//Keyboard and mouse test.
 		if (keysDown[32])
 			colour = 'blue';
 		ctx.fillStyle = colour;
 		ctx.fillRect(mouse.x - 20, mouse.y - 30, 40, 60);
-		
-		setTimeout(requestAnimationFrame(gameLoop), fps - (Date.now() - frameBegin));
+
+		//Update timers and restart issue a request to render the pending frame.
+		deltaTime = performance.now() - timestamp;
+		setTimeout(requestAnimationFrame(gameLoop), millisecondsPerFrame - deltaTime);
 	}
 	requestAnimationFrame(gameLoop);
 
