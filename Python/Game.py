02062b09981d58462fa41d0fd7bb74b0e3cf5190
Connor Smiley <connor.smiley@uoit.net>
2021-07-28 14:25:42 -0400

    Made simple scene classes and tested inheritance/polymorphism

--- Python/Game.py
+++ Python/Game.py
@@ -4,6 +4,55 @@ pg.init()
 canvas = pg.display.set_mode([640, 480])
 ctx = pg.display.get_surface()
 
+class Scene:
+    def __init__(self):
+        pass
+
+    def onStart(self):
+        print("Default start")
+    
+    def onFinish(self):
+        print("Default finish")
+
+class BeginScene(Scene):
+    def __init__(self):
+        super().__init__()
+    
+    def onStart(self):
+        print("Begin start")
+    
+    def onFinish(self):
+        print("Begin finish")
+
+class MiddleScene(Scene):
+    def __init__(self):
+        super().__init__()
+    
+    def onStart(self):
+        print("Middle start")
+    
+    def onFinish(self):
+        print("Middle finish")
+
+class EndScene(Scene):
+    def __init__(self):
+        super().__init__()
+
+    def onStart(self):
+        print("End start")
+    
+    def onFinish(self):
+        print("End finish")
+
+#Runtime testing to make sure there's no instantiation errors.
+a = BeginScene()
+b = MiddleScene()
+c = EndScene()
+
+a.onStart()
+b.onStart()
+c.onStart()
+
 while 1:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
