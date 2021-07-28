aed803b406c54f5f01bf8aba633463856f11f150
Connor Smiley <connor.smiley@uoit.net>
2021-07-28 01:51:01 -0400

    Setup pygame screen

--- Python/Game.py
+++ Python/Game.py
@@ -1,4 +1,12 @@
-def game():
-    print("Oh no, here comes the snake... SSSSSSSSSSSSSSSSSSSSssssssssssssssssssssssssssssssss")
+import sys, pygame
+pygame.init()
 
-game()
+canvas = pygame.display.set_mode([640, 480])
+ctx = pygame.display.get_surface()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    canvas.fill(pygame.Color(0, 0, 0))
+    pygame.display.flip()
