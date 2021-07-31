1fd57fce0cd6e2494ed1f02c580053b89a6932c1
Connor Smiley <connor.smiley@uoit.net>
2021-07-31 01:13:40 -0400

    Was rendering my button before clearing the screen...

--- Python/Game.py
+++ Python/Game.py
@@ -1,53 +1,11 @@
 import sys, pygame as pg
 
-class Scene:
-    def __init__(self):
-        pass
-
-    def onStart(self):
-        print("Default start")
-    
-    def onFinish(self):
-        print("Default finish")
-
-class BeginScene(Scene):
-    def __init__(self):
-        super().__init__()
-    
-    def onStart(self):
-        print("Begin start")
-    
-    def onFinish(self):
-        print("Begin finish")
-
-class MiddleScene(Scene):
-    def __init__(self):
-        super().__init__()
-    
-    def onStart(self):
-        print("Middle start")
-    
-    def onFinish(self):
-        print("Middle finish")
-
-class EndScene(Scene):
-    def __init__(self):
-        super().__init__()
-
-    def onStart(self):
-        print("End start")
-    
-    def onFinish(self):
-        print("End finish")
-
 def drawText(text, x, y, colour, font, surface):
     textRender = font.render(text, True, colour)
     textRect = textRender.get_rect()
     textRect.center = (x, y)
     surface.blit(textRender, textRect)
 
-#pygame has a Rect class that uses way too much memory, but it handles collisions for me.
-#Writing this is already painful enough so I've decided to use its silly Rect class...
 class Button(pg.Rect):
     def __init__(self, x, y, w, h, button_colour, onClickHandler):
         super().__init__(x, y, w, h)
@@ -71,45 +29,90 @@ class TextButton(Button):
         super().draw(surface)
         drawText(text, self.x + self.w / 2, self.y + self.h / 2, self.text_colour, self.font, surface)
 
-pg.init()
-
-screen = pg.display.set_mode([640, 480])
-surface = pg.display.get_surface()
-mouse = (0, 0)
-mouseDown = False
-
+#Creating globals here because the scene classes need them.
 black = pg.Color(0, 0, 0)
 red = pg.Color(255, 0, 0)
 green = pg.Color(0, 255, 0)
 blue = pg.Color(0, 0, 255)
 white = pg.Color(255, 255, 255)
 
-#Runtime testing to make sure there's no instantiation errors.
-a = BeginScene()
-b = MiddleScene()
-c = EndScene()
-
-a.onStart()
-b.onStart()
-c.onStart()
-
+pg.init()
 font10 = pg.font.Font('freesansbold.ttf', 10)
 font20 = pg.font.Font('freesansbold.ttf', 20)
 font30 = pg.font.Font('freesansbold.ttf', 30)
 
-button = Button(0, 0, 60, 40, red, lambda : print("Button!"))
-textButton = TextButton(0, 0, 60, 40, red, white, font10, lambda : print("Text button!"))
+class Scene:
+    def __init__(self):
+        pass
+
+    def onStart(self):
+        print("Default start")
+    
+    def onFinish(self):
+        print("Default finish")
+
+    def onUpdate(self, surface):
+        print("Default update")
+
+    def onClick(self, x, y):
+        print("Default click")
+
+class BeginScene(Scene):
+    def __init__(self):
+        super().__init__()
+        self.button = TextButton(0, 0, 60, 40, red, white, font10, lambda : print("Begin button!"))
+    
+    def onStart(self):
+        print("Begin start")
+    
+    def onFinish(self):
+        print("Begin finish")
+
+    def onUpdate(self, surface):
+        self.button.draw("Memes", surface)
+
+    def onClick(self, x, y):
+        self.button.onClick(x, y)
+
+class MiddleScene(Scene):
+    def __init__(self):
+        super().__init__()
+    
+    def onStart(self):
+        print("Middle start")
+    
+    def onFinish(self):
+        print("Middle finish")
+
+class EndScene(Scene):
+    def __init__(self):
+        super().__init__(surface)
+
+    def onStart(self):
+        print("End start")
+    
+    def onFinish(self):
+        print("End finish")
+
+screen = pg.display.set_mode([640, 480])
+surface = pg.display.get_surface()
+mouse = (0, 0)
+mouseDown = False
+
+scene = BeginScene()
 
 while 1:
-    for event in pg.event.get():
-        if event.type == pg.QUIT: sys.exit()
-    
-    #I swear there's a way to convert from tuple to Point in one line but I'm not good enough at Python to know how.
     mouse = pg.mouse.get_pos()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit()
 
+        elif event.type == pg.MOUSEBUTTONUP:
+            scene.onClick(mouse[0], mouse[1])
+
+    #Do not put any rendering code above this!
     screen.fill(pg.Color(0, 0, 0))
-
-    textButton.draw("Memes", surface)
+    scene.onUpdate(surface)
 
     pg.draw.rect(surface, red, pg.Rect(320, 240, 60, 40))
     drawText("Memes", 350, 260, white, font10, surface)
